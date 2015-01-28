80c168f7139c23559c8050016256a629bcfbdf5f
Ramon De La Torre <delatorre.ramon66@gmail.com>
2015-01-28 15:58:21 -0800

    Commit number 2 for blog.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Web Server is running locally";
+        echo "The Web Server is running locally";
         ?>
     </body>
 </html>
