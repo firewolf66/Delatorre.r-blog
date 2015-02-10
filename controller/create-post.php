4b88903d87bce2f1d7ba0269e82825514342cf54
Ramon De La Torre <delatorre.ramon66@gmail.com>
2015-02-10 14:03:20 -0800

    Commit number 8-9 fir blog.

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,7 @@
 <?php
 
-$title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-$post = filer_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
 echo "<p>Title: $title</p>";
 echo "<p>Post: $post</p>";
