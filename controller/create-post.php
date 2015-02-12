36079688a7e98e60c609e32d98b1898b3c1c4d17
Ramon De La Torre <delatorre.ramon66@gmail.com>
2015-02-12 14:03:16 -0800

    Commit number 11-12 for blog.

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,11 +1,17 @@
 <?php
 
-require_once(__DIR__ . "/../model/database.php");
+require_once(__DIR__ . "/../model/config.php");
 
 $connection = new mysqli($host, $username, $password, $database);
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-$query = $connection->query("INSERT INTO posts SET title = $title");
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+if ($query) {
+    echo "<p>Successfully inserteted post: $title</p>";
+} else {
+    echo "<p>connection->error</p>";
+}
 
 $connection->close();
--- model/config.php
+++ model/config.php
@@ -2,3 +2,8 @@
 
 $path = "/Delatorre.r-blog/";
 
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database = "blog_db";
+
