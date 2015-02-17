ea36b0f15995fe113411171d3f56b7143417bbba
Ramon De La Torre <delatorre.ramon66@gmail.com>
2015-02-17 14:04:17 -0800

    Commit number 11-12 for blog.

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,27 +2,6 @@
 
 require_once(__DIR__ . "/../model/config.php");
 
-$connection = new mysqli($host, $username, $password);
-
-if ($connection->connect_error) {
-    die("<p>Error: " . $connection->connect_error . "</p>");
-}
-
-$exists = $connection->select_db($database);
-
-echo $exists;
-
-if (!$exists) {
-    $query = $connection->query("CREATE DATABASE $database");
-
-
-    if ($query) {
-        echo "<p>Successfully created database: " . $database . "</p>";
-    }
-} else {
-    echo "<p>Database already exists.</p>";
-}
-
 $query = $connection->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar(255) NOT NULL,"
@@ -35,4 +14,3 @@ if ($query) {
     echo "<p>$connection->error</p>";
 }
 
-$connection->close();
--- model/config.php
+++ model/config.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once(__DIR__ . "/database.php");
+
 $path = "/Delatorre.r-blog/";
 
 $host = "localhost";
 $username = "root";
 $password = "root";
 $database = "blog_db";
+
+$connection = new Database($host, $username, $password, $database);
 
